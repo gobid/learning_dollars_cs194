a5d5e57dcd6476f6151a3151f4bdfff8b245bcca
N0tinuse <wmeyer@stanford.edu>
2014-01-22 20:23:30 -0800

    added ocw test code in its own folder, will need to be changed/integrated at some point

---
+++ ocw_test_code/ocwtest.py
@@ -0,0 +1,12 @@
+import urllib
+from oauth2client.tools import argparser
+if __name__ == "__main__":
+  argparser.add_argument("--q", help="Search term", default="Google")
+  args = argparser.parse_args()
+  query = "http://www.ocwsearch.com/api/v1/search.json?q="
+  query += args.q
+  query += "&page=1&contact=http%3a%2f%2fwww.penis.com"
+  f = urllib.urlopen(query)
+  s = f.read()
+  print s
+  f.close()
