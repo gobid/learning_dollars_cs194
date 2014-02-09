4beb066e5a5fca98923091cd4d97a9535042f32e
leorong <leorong@stanford.edu>
2014-02-09 01:48:04 -0800

    created front end for send message in dashboard

--- learningdollars.py
+++ learningdollars.py
@@ -91,7 +91,7 @@ application = webapp2.WSGIApplication([
         name='createmilestonepayment'
     ),
     webapp2.Route(
-        '/sendMessage/<project_id:\d+>/<message_text:\w+>/<user_name:\w+>',
+        '/sendMessage/<project_id:\d+>/<message_text:[^/]+>/<user_name:\w+>',
         handler=SendMessage,
         name='sendMessage'
     ),
--- static/views/js/dashboard.js
+++ static/views/js/dashboard.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	// My Posts handlers (Tim & Govi)
 
-	$.get('/getprojectbids/8', function(data){
+	$.get('/getprojectbids/1034', function(data){
 		bids = data['json-result']['items']
 		for (var b in bids){
 			$('#bids-on-post').append('<li>' + bids[b].descr + ' ' 
@@ -14,4 +14,25 @@ $(document).ready(function() {
 
 	// Mailbox handlers (Leo)
 
+	$('#messageTabs a').click(function (e) {
+	  	e.preventDefault()
+	  	$(this).tab('show')
+	})
+
+	$.get('/inboxMessages', function(data) {
+		messages = data['json-result']['items']
+		
+	})
+
+	$('#sendBtn').click(function (e) {
+		projectId = $('#project_id').val()
+		messageText = $('#message_text').val()
+		toUserName = $('#to_user_name').val()
+		url = '/sendMessage/1034/' + messageText + '/' + toUserName
+		window.console&&console.log(url)
+
+		$.get(url, function(data) {
+			$('#newMessage').append(data)
+		})
+	})
 })
